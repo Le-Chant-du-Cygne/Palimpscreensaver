b3abe39e457a6e67487b376aa6b27ab56ed15461
Simon Chauvin <contact@simonchauvin.com>
2016-01-24 14:42:42 +0100

    Game manager for cursor and quit.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+	void Start ()
+    {
+        Cursor.visible = false;
+	}
+	
+	void Update ()
+    {
+	    if (Input.anyKeyDown)
+        {
+            Application.Quit();
+        }
+	}
+}
