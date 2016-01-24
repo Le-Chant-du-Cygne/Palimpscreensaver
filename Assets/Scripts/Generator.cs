4a14098c295308e65aa878a6590fc0d4f40b86a3
Simon Chauvin <contact@simonchauvin.com>
2016-01-24 14:09:00 +0100

    Prefab and script for basic working.

---
+++ Assets/Scripts/Generator.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generator : MonoBehaviour
+{
+    public Transform corridor;
+    public float corridorGenerationTime;
+    public int maxScale;
+
+    private Transform corridors;
+    private Texture[] textures;
+
+    private float corridorGenerationTimer;
+    private Vector3 currentPosition;
+
+	void Start ()
+    {
+        corridors = GameObject.Find("Corridors").transform;
+        textures = Resources.LoadAll<Texture>("Textures/");
+
+        corridorGenerationTimer = 0f;
+        currentPosition = Vector3.zero;
+	}
+	
+	void Update ()
+    {
+	    if (corridorGenerationTimer >= corridorGenerationTime)
+        {
+            spawnRandomCorridor();
+            corridorGenerationTimer = 0f;
+        }
+        corridorGenerationTimer += Time.deltaTime;
+	}
+
+    void spawnRandomCorridor()
+    {
+        Transform newCorridor = Instantiate(corridor, Vector3.zero, Quaternion.identity) as Transform;
+        newCorridor.parent = corridors;
+        Vector3 scale = new Vector3(1f, 1f, Random.Range(1, maxScale + 1));
+        newCorridor.localScale = scale;
+        newCorridor.position = currentPosition;
+        currentPosition = new Vector3(0f, 0f, currentPosition.z);
+        currentPosition += new Vector3(0f, 0f, 4f * scale.z);
+
+        // Texture
+        MeshRenderer[] renderers = newCorridor.GetComponentsInChildren<MeshRenderer>();
+        Color color1 = Random.ColorHSV(),
+            color2 = Random.ColorHSV(),
+            color3 = Random.ColorHSV();
+        Texture randomTexture1 = textures[Random.Range(0, textures.Length - 1)];
+        Texture randomTexture2 = textures[Random.Range(0, textures.Length - 1)];
+        foreach (MeshRenderer renderer in renderers)
+        {
+            renderer.material.SetColor("_Color1", color1);
+            renderer.material.SetColor("_Color2", color2);
+            renderer.material.SetColor("_Color3", color3);
+
+            renderer.material.SetTexture("_Illum", randomTexture1);
+            renderer.material.SetTexture("_Illum2", randomTexture2);
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float minSpeed;
+    public float maxSpeed;
+    public float minTimeToEnd;
+    public float maxTimeToEnd;
+
+    private Generator generator;
+    private Transform corridors;
+
+    private Vector3 lastPosition;
+    private float currentSpeed;
+    private float currentMaxSpeed;
+    private float currentTimeToEnd;
+    private float endTimer;
+
+	void Start ()
+    {
+        generator = GameObject.FindObjectOfType<Generator>();
+        corridors = GameObject.Find("Corridors").transform;
+
+        lastPosition = Vector3.zero;
+        currentSpeed = 0f;
+        currentMaxSpeed = Random.Range(minSpeed, maxSpeed);
+        currentTimeToEnd = Random.Range(minTimeToEnd, maxTimeToEnd);
+        endTimer = 0f;
+    }
+	
+	void Update ()
+    {
+        // Movement
+        float currentSpeed = Interpolate.Ease(Interpolate.EaseType.EaseOutQuad)(0f, currentMaxSpeed, endTimer, currentTimeToEnd);
+        transform.Translate(new Vector3(0f, 0f, currentSpeed) * Time.deltaTime);
+        endTimer += Time.deltaTime;
+
+        // Reset parameters
+        if (endTimer >= currentTimeToEnd)
+        {
+            currentMaxSpeed = Random.Range(minSpeed, maxSpeed);
+            currentTimeToEnd = Random.Range(minTimeToEnd, maxTimeToEnd);
+            endTimer = 0f;
+        }
+
+        // Destroy old corridors
+        if (transform.position.z - lastPosition.z > 4f * generator.maxScale)
+        {
+            Destroy(corridors.GetChild(0).gameObject);
+            lastPosition = transform.position;
+        }
+	}
+}
