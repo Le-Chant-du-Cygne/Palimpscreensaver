4420e76b59e0fc240beb8e6e629ebd761f224c2e
Simon Chauvin <contact@simonchauvin.com>
2016-01-24 21:08:33 +0100

    Two phases interpolation for smoothness.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -16,6 +16,7 @@ public class Movement : MonoBehaviour
     private float currentMaxSpeed;
     private float currentTimeToEnd;
     private float endTimer;
+    private bool accelerating;
 
 	void Start ()
     {
@@ -27,20 +28,37 @@ public class Movement : MonoBehaviour
         currentMaxSpeed = Random.Range(minSpeed, maxSpeed);
         currentTimeToEnd = Random.Range(minTimeToEnd, maxTimeToEnd);
         endTimer = 0f;
+        accelerating = true;
     }
 	
 	void Update ()
     {
         // Movement
-        float currentSpeed = Interpolate.Ease(Interpolate.EaseType.EaseOutQuad)(minSpeed, currentMaxSpeed, endTimer, currentTimeToEnd);
+        float currentSpeed = 0f;
+        if (accelerating)
+        {
+            currentSpeed = Interpolate.Ease(Interpolate.EaseType.EaseInQuad)(minSpeed, currentMaxSpeed, endTimer, currentTimeToEnd);
+        }
+        else
+        {
+            currentSpeed = Interpolate.Ease(Interpolate.EaseType.EaseInQuad)(currentMaxSpeed, minSpeed, endTimer, currentTimeToEnd);
+        }
         transform.Translate(new Vector3(0f, 0f, currentSpeed) * Time.deltaTime);
         endTimer += Time.deltaTime;
 
         // Reset parameters
         if (endTimer >= currentTimeToEnd)
         {
-            currentMaxSpeed = Random.Range(minSpeed, maxSpeed);
-            currentTimeToEnd = Random.Range(minTimeToEnd, maxTimeToEnd);
+            if (accelerating)
+            {
+                accelerating = false;
+            }
+            else
+            {
+                currentMaxSpeed = Random.Range(minSpeed, maxSpeed);
+                currentTimeToEnd = Random.Range(minTimeToEnd, maxTimeToEnd);
+                accelerating = true;
+            }
             endTimer = 0f;
         }
 
