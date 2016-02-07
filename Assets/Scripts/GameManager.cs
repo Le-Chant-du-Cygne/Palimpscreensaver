04163deedc6ad17e3a6e5a69a11e324eb3324bfa
Simon Chauvin <contact@simonchauvin.com>
2016-02-07 21:02:17 +0100

    Quit if mouse moves.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,17 +3,20 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour
 {
+    private Vector3 lastMousePosition;
 
 	void Start ()
     {
         Cursor.visible = false;
+        lastMousePosition = Input.mousePosition;
 	}
 	
 	void Update ()
     {
-	    if (Input.anyKeyDown)
+	    if (Input.anyKeyDown || Input.mousePosition != lastMousePosition || Input.GetMouseButton(0) || Input.GetMouseButton(1) || Input.mouseScrollDelta.magnitude > 0f)
         {
             Application.Quit();
         }
+        lastMousePosition = Input.mousePosition;
 	}
 }
